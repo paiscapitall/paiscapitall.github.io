46223036c3054fcfbe66069a7621707302a78323
paiscapitall <xxxfxp@gmail.com>
2025-01-17 07:51:39 +0800

    Create link.js
    Yes

---
+++ src/utils/link.js
@@ -0,0 +1,20 @@
+const socialLinks = {
+  instagram: "https://www.instagram.com/paiscapital",
+  tiktok: "https://www.tiktok.com/@paiscapital",
+  youtube: "https://youtube.com/@paiscapital101",
+  rootMe: "https://www.root-me.org/Meguminz",
+  cryptoHack: "https://cryptohack.org/user/Meguminz/",
+  codeforces: "https://codeforces.com/profile/Meguminz",
+  tryHackMe: "https://tryhackme.com/r/p/Meguminz",
+  email: "mailto:admin@gmail.com",
+  hireMe: "mailto:admin@gmail.com",
+  downloadCV: "/cv.pdf"
+};
+
+const navigationLink = {
+  home: '#',
+  project: '#project',
+  about: '#about',
+  blog: '/blog', // Mengarahkan ke halaman /blog  
+  connect: '#connect'
+};
